a27a6d9a796557ec10c355692c591b5c0192d805
Estudiantes UPB <estudiante.upb@bga.upb>
2025-10-01 20:50:09 -0500

    creacion archivo libros

---
+++ libros.js
@@ -0,0 +1,25 @@
+let libros = [{
+    titulo : "Cien años de soledad",
+    autor : "Gabriel Garcia Marquez",
+    estado : "prestado"
+},
+{
+    titulo : "Don Quijote de la Mancha",
+    autor : "Miguel de Cervantes",
+    estado : "disponible"
+}
+]
+export function crear_libros(titulo, autor, estado){
+let nuevo_libro = {
+    
+    titulo :  titulo,
+    autor : autor,
+    estado : estado,
+}
+libros.push(nuevo_libro)
+}
+
+crear_libros("El hobbit", "JRR TOLKIEN", "Disponible")
+console.log(libros)
+
+
