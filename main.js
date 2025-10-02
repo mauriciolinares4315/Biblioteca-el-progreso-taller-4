76287efee597eec50203eb2e8bd0d523d877cad9
AngeloGN <angelo.gonzalez@upb.edu.co>
2025-10-01 19:55:10 -0500

    main con arrays de prueba

---
+++ main.js
@@ -0,0 +1,19 @@
+// array de prueba
+let libros = [{
+    titulo : "Cien años de soledad",
+    autor : "Gabriel Garcia Marquez",
+    estado : "prestado"
+},
+{
+    titulo : "Don Quijote de la Mancha",
+    autor : "Miguel de Cervantes",
+    estado : "disponible"
+}
+]
+// array de prueba
+let usuarios = [
+{ 
+    nombre : "Carlos Amaya",
+    correo : "Carlosamaya@gmail.com"
+}
+]
